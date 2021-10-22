03e64a9d7278a564515ee0a6339c1bd3c0f08add
Sagar <mandal.sagar79@gmail.com>
2021-10-22 16:20:43 +0530

    done with episode 14, revise context in golang

---
+++ video14/product-api/handlers/get.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/nicholasjackson/building-microservices-youtube/product-api/currencies/protos/currency"
+	"github.com/nicholasjackson/building-microservices-youtube/product-api/data"
+)
+
+// swagger:route GET /products products listProducts
+// Return a list of products from the database
+// responses:
+//	200: productsResponse
+
+// ListAll handles GET requests and returns all current products
+func (p *Products) ListAll(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("[DEBUG] get all records")
+
+	prods := data.GetProducts()
+
+	err := data.ToJSON(prods, rw)
+	if err != nil {
+		// we should never be here but log the error just incase
+		p.l.Println("[ERROR] serializing product", err)
+	}
+}
+
+// swagger:route GET /products/{id} products listSingleProduct
+// Return a list of products from the database
+// responses:
+//	200: productResponse
+//	404: errorResponse
+
+// ListSingle handles GET requests
+func (p *Products) ListSingle(rw http.ResponseWriter, r *http.Request) {
+	id := getProductID(r)
+
+	p.l.Println("[DEBUG] get record id", id)
+
+	prod, err := data.GetProductByID(id)
+
+	switch err {
+	case nil:
+
+	case data.ErrProductNotFound:
+		p.l.Println("[ERROR] fetching product", err)
+
+		rw.WriteHeader(http.StatusNotFound)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	default:
+		p.l.Println("[ERROR] fetching product", err)
+
+		rw.WriteHeader(http.StatusInternalServerError)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	}
+
+	// get exchange rate
+	rr := currency.RateRequest{
+		Base:        currency.Currencies(currency.Currencies_value["EUR"]),
+		Destination: currency.Currencies(currency.Currencies_value["GBP"]),
+	} // type conversion int32
+	resp, err := p.cc.GetRate(context.Background(), &rr) // what does context do, still doubt
+	if err != nil {
+		p.l.Println("[ERROR] error getting new rate", err)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	}
+
+	prod.Price = prod.Price * resp.Rate
+
+	err = data.ToJSON(prod, rw)
+	if err != nil {
+		// we should never be here but log the error just incase
+		p.l.Println("[ERROR] serializing product", err)
+	}
+}
